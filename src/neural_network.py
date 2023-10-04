b292496018de8c38018843a335f1e54620905c76
Gautherot <mo.gautherot@laposte.net>
2023-10-05 00:07:01 +0200

    add the fit function into neural_network.py

--- src/neural_network.py
+++ src/neural_network.py
@@ -5,8 +5,7 @@ import numpy as np
 from src.performance_metrics import logloss, accuracy
 
 class NeuralNetwork():
-    
-    
+        
     def __init__(self, list_architecture:list)->None:
         """Initialization of the architecture of the Neural Network"""
         self.architecture = dict()
@@ -28,3 +27,25 @@ class NeuralNetwork():
         
         for _, layer in reversed(self.architecture.items()):
             da_prev = layer.compute_grad(da_prev)
+
+    def parameters_update(self, learning_rate:float=0.001)->None:
+        """Update every parameters of the network layer per layer"""
+        
+        for _, layer in self.architecture.items():
+            layer.parameters_update(learning_rate)
+
+    def fit(self, x_train:np.ndarray, y_train:np.ndarray, epochs:int=1, learning_rate:float=0.001)->None:
+        """Train the neural network with the training set x_train to approach y_train"""
+        
+        for i in range(epochs):
+            
+            y_pred = self.predict(x_train)
+            
+            self.compute_grad(y_train, y_pred)
+            
+            self.parameters_update(learning_rate=learning_rate)
+            
+            loss = logloss(y_train, y_pred)
+            accu = accuracy(y_train, y_pred)
+            
+            print('Epoch : '+str(i)+' - loss : '+str(loss)+' - accuracy : '+str(accu))
--- tests/test_neural_network.py
+++ tests/test_neural_network.py
@@ -2,6 +2,7 @@
 """ Test the function inside class of neural_network.py """
 
 from src.neural_network import NeuralNetwork
+from src.performance_metrics import logloss, accuracy
 from src.layers import Dense
 import numpy as np
 
@@ -54,3 +55,64 @@ def test_compute_grad()-> None:
     assert model.architecture[2].dw.shape == (10, 1)
     assert model.architecture[2].db.shape == (1,)
 
+## Unit test for the parameters update function
+
+def test_parameters_update()-> None:
+    """Test the update of every parameters of the network"""
+    # randomness is fixed
+    np.random.seed(seed=123)
+    # Initialization of the network
+    model = NeuralNetwork([Dense(2, 20, 'relu'), 
+                              Dense(20, 10, 'relu'), 
+                              Dense(10, 1, 'sigmoid')])
+    
+    # Generation of the input data
+    input_data = np.array([[0, 0], [2, 2], [5, 5], [10, 10]])
+    # Generation of the true data labels
+    true_label = np.array([[0], [0], [1], [1]])    
+
+    # predict before update
+    predict_label_before = model.predict(input_data)
+
+    # Compute the loss before update the parameters
+    loss_before_parameters_update = logloss(true_label, predict_label_before)
+
+    # compute gradient for every layers
+    model.compute_grad(true_label, predict_label_before)
+
+    # update the parameters for every layers
+    model.parameters_update(learning_rate=0.1)
+
+    # predict after update
+    predict_label_after = model.predict(input_data)
+
+    # Compute the loss after update the parameters
+    loss_after_parameters_update = logloss(true_label, predict_label_after)
+
+    assert loss_before_parameters_update > loss_after_parameters_update
+
+def test_fit_neural_network()-> None:
+    """Test the train of a neural network model"""
+
+    # Generate explanatory variable
+    x_linear = np.random.randn(100, 2)
+    x_linear[:, 1] += 10
+    x_linear = np.concatenate([np.random.randn(100, 2), x_linear])
+
+    # Generate labels
+    y_linear = np.zeros(100)
+    y_linear = np.concatenate([np.ones(100), y_linear])
+    y_linear = y_linear.reshape(-1, 1)
+
+    # Data normalization 
+    x_train_norm = (x_linear - np.mean(x_linear, axis=0))/(np.std(x_linear, axis=0))
+
+    model = NeuralNetwork([ Dense(2, 1, 'sigmoid')])
+
+    model.fit(x_train_norm, y_linear, 10, learning_rate=0.001)
+
+    y_pred = model.predict(x_train_norm)
+
+    model_accuracy = accuracy(y_linear, y_pred)
+    
+    assert 1 == model_accuracy
