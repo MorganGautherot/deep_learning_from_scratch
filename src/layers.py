8d8a0dc6f1e67e66b399324819650df996117e6b
Gautherot <mo.gautherot@laposte.net>
2023-10-05 09:37:27 +0200

    linting update

--- src/layers.py
+++ src/layers.py
@@ -4,68 +4,71 @@
 import numpy as np
 from src.activation_functions import relu, sigmoid
 
-class Dense():
-    
-    def __init__(self, neurons_input:int, neurons_output:int, activation:str=None) -> None:
-        """ This function save core information to initialize the parameters and the bias of the layer"""
-        self.parameters = np.random.random([neurons_input, neurons_output])*0.01
+
+class Dense:
+    def __init__(
+        self, neurons_input: int, neurons_output: int, activation: str = None
+    ) -> None:
+        """This function save core information to initialize the parameters and
+        the bias of the layer"""
+        self.parameters = np.random.random([neurons_input, neurons_output]) * 0.01
         self.bias = np.zeros(neurons_output)
-        
-        if activation == 'sigmoid':
-            self.activation = 'sigmoid'
+
+        if activation == "sigmoid":
+            self.activation = "sigmoid"
             self.activation_function = sigmoid
-        elif activation == 'relu':
-            self.activation ='relu'
+        elif activation == "relu":
+            self.activation = "relu"
             self.activation_function = relu
-        else :
-            self.activation = 'linear'
+        else:
+            self.activation = "linear"
             self.activation_function = lambda x: x
 
-    def predict(self, a_prev:np.ndarray)->np.ndarray:
-        """This function compute a matrix multiplication between a_prev and the parameters of the model"""
-        
-        self.a_prev = a_prev 
-        
+    def predict(self, a_prev: np.ndarray) -> np.ndarray:
+        """This function compute a matrix multiplication between a_prev and
+        the parameters of the model"""
+
+        self.a_prev = a_prev
+
         self.z = np.dot(self.a_prev, self.parameters) + self.bias
-        
+
         self.a = self.activation_function(self.z)
-        
+
         return self.a
-    
-    def activation_function_derivative(self, da:np.array)->np.ndarray:
+
+    def activation_function_derivative(self, da: np.array) -> np.ndarray:
         """Compute de deritave of the activation function"""
 
-        if self.activation == 'relu':
-            
+        if self.activation == "relu":
             dz = da
-            dz[self.z<=0] = 0
-            
-        elif self.activation == 'sigmoid':
-            
-            s = 1/(1+np.exp(-self.z))
-            dz = da * s * (1-s)
-            
-        else :
+            dz[self.z <= 0] = 0
+
+        elif self.activation == "sigmoid":
+            s = 1 / (1 + np.exp(-self.z))
+            dz = da * s * (1 - s)
+
+        else:
             dz = da
 
         return dz
-    
-    def compute_grad(self, da:np.ndarray)->np.ndarray:
+
+    def compute_grad(self, da: np.ndarray) -> np.ndarray:
         """Compute gradient for every parameters of this layer"""
-        
+
         dz = self.activation_function_derivative(da)
-            
+
         m = self.a.shape[1]
-        
-        self.dw = (1/m) * np.dot(self.a_prev.T, dz)
-        self.db = (1/m) * np.sum(dz, axis=0)
-        
+
+        self.dw = (1 / m) * np.dot(self.a_prev.T, dz)
+        self.db = (1 / m) * np.sum(dz, axis=0)
+
         da_prev = np.dot(dz, self.parameters.T)
-        
+
         return da_prev
 
-    def parameters_update(self, learning_rate:float)->None:
-        """Update the values of the parameters using gradient descent algorithm"""
-        
+    def parameters_update(self, learning_rate: float) -> None:
+        """Update the values of the parameters using gradient
+        descent algorithm"""
+
         self.parameters = self.parameters - learning_rate * self.dw
         self.bias = self.bias - learning_rate * self.db
--- src/performance_metrics.py
+++ src/performance_metrics.py
@@ -3,15 +3,21 @@
 
 import numpy as np
 
-def logloss(y:np.ndarray, y_pred:np.ndarray)->float:
-    """Compute logloss between y and y_pred"""
-    
-    m = y.shape[0]
-    return (-1/m) * np.sum((1-y)* np.log(1-y_pred+1e-6) + y * np.log(y_pred+1e-6))
 
-def accuracy(y:np.ndarray, y_pred:np.ndarray, threshold:float=0.5)->float:
-    """Compute accuracy between y and y_pred"""
-    
+def logloss(y: np.ndarray, y_pred: np.ndarray) -> float:
+    """Compute logloss between y and y_pred"""
+
     m = y.shape[0]
-    y_pred_hard = np.where(y_pred>=threshold, 1, 0)
-    return (1/m) * np.sum(y_pred_hard == y)
+    return (-1 / m) * np.sum(
+        (1 - y) * np.log(1 - y_pred + 1e-6) + y * np.log(y_pred + 1e-6)
+    )
+
+
+def accuracy(y: np.ndarray, 
+             y_pred: np.ndarray, 
+             threshold: float = 0.5) -> float:
+    """Compute accuracy between y and y_pred"""
+
+    m = y.shape[0]
+    y_pred_hard = np.where(y_pred >= threshold, 1, 0)
+    return (1 / m) * np.sum(y_pred_hard == y)
